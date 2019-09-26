58f7060a4fb4051bc4480e89b15075578fa26181
Charity Parks <tuppercharity@gmail.com>
2019-09-26 19:46:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  
+  attr_accessor :school_name, :roster 
+  
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+    
+ end 
+  def add_student(student_name, grade)
+    
+  end
+  
+  
+  def grade
+    
+  end
+  
+  
+  def sort 
+    
+  end
+  
+ 
+end
