0cb0dd5f4fa854732146d2fa9e2c9ec87e12f1d3
Charity Parks <tuppercharity@gmail.com>
2019-09-27 00:12:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,29 @@
 class School
   
-  attr_accessor :school_name, :roster 
+  attr_accessor :roster 
   
   
-  def initialize(school_name)
-    @school_name = school_name
+  def initialize(name)
+    @name = name
     @roster = {}
     
  end 
   def add_student(student_name, grade)
-    
+    if !roster[grade]
+        roster[grade] = []
   end
   
+  roster[grade] << student_name
+end
   
-  def grade
-    
+  def grade(grade)
+    roster[grade]
   end
   
-  
-  def sort 
+  def sort
+    roster.each do |grade, student|
+      roster[grade] = student.sort
+    end
     
   end
   
